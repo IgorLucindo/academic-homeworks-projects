21bc990082828fd6af11c188ebee37e1d847846a
IgorLucindo <lucindoigor@yahoo.com.br>
2025-02-20 00:06:00 -0600

    1

--- homeworks/operations-research/HW2/problem1_4.py
+++ homeworks/operations-research/HW2/problem1_4.py
@@ -1,5 +1,6 @@
 from utils.instance_utils import *
 from utils.solve_utils import *
+import time
 
 
 def main():
@@ -11,12 +12,29 @@ def main():
 
     # solve every instance using each method
     for Q, A, c, b in instances:
-        x1, u1 = analytical_solver(Q, A, c, b, dim)
+        # get start time
+        time0 = time.time()
+
+        # run each solver
+        x1, u1 = analytical_solver(Q, A, c, b)
+        time1 = time.time()
         x2, u2 = gradient_descent_solver(Q, A, c, b, dim)
+        time2 = time.time()
+        x3, u3 = newton_method_solver(Q, A, c, b, dim)
+        time3 = time.time()
+        x4, u4 = linear_system_solver(Q, A, c, b, dim)
+        time4 = time.time()
+
+        # get running time of each solver
+        running_time1 = time1 - time0
+        running_time2 = time2 - time1
+        running_time3 = time3 - time2
+        running_time4 = time4 - time3
 
         # print results for each solver
-        print(f"x: \n{x1.T}, \n{x2.T}\n")
-        print(f"u: \n{u1.T}, \n{u2.T}\n")
+        print(f"running time: \n{running_time1}, \n{running_time2}, \n{running_time3}, \n{running_time4}\n")
+        print(f"x: \n{x1.T}, \n{x2.T}, \n{x3.T}, \n{x4.T}\n")
+        print(f"u: \n{u1.T}, \n{u2.T}, \n{u3.T}, \n{u4.T}\n")
 
 
 if __name__ == "__main__":
--- homeworks/operations-research/HW2/utils/solve_utils.py
+++ homeworks/operations-research/HW2/utils/solve_utils.py
@@ -2,10 +2,70 @@ import numpy as np
 
 
 # solve analyticaly from KKT Conditions method
-def analytical_solver(Q, A, c, b, dim):
+def analytical_solver(Q, A, c, b):
+    Q_inv = np.linalg.inv(Q)
+    # getting u
+    u = -np.linalg.inv(A @ Q_inv @ A.T)@(b + A@Q_inv@c)
+
+    # get value of x based on u
+    x = -Q_inv@(c + A.T@u)
+
+    # return variables x and u
+    return x, u
+
+
+# solve by gradient descent from Lagrangian Duality method
+def gradient_descent_solver(Q, A, c, b, dim, alpha=0.01, tol=1e-6, max_iter=1000):
+    # precompute fixed terms
+    Q_inv = np.linalg.inv(Q)
+    g = A@Q_inv@A.T
+    h = A@Q_inv@c + b
+    gradient = lambda u: g@u + h
+
+    # gradient descent
+    u = np.zeros((dim, 1))
+    for i in range(max_iter):
+        delta = alpha * gradient(u)
+        u -= delta
+        if np.linalg.norm(delta) < tol:
+            break
+
+    # get value of x based on u
+    x = -Q_inv@(c + A.T@u)
+
+    # return variables x and u
+    return x, u
+
+
+# solve by newton's method from Lagrangian Duality method
+def newton_method_solver(Q, A, c, b, dim, tol=1e-6, max_iter=1000):
+    # precompute fixed terms
+    Q_inv = np.linalg.inv(Q)
+    g = A@Q_inv@A.T
+    h = A@Q_inv@c + b
+    gradient = lambda u: g@u + h
+    H_inv = np.linalg.inv(g)
+
+    # newton's method
+    u = np.zeros((dim, 1))
+    for i in range(max_iter):
+        delta = H_inv @ gradient(u)
+        u -= delta
+        if np.linalg.norm(delta) < tol:
+            break
+
+    # get value of x based on u
+    x = -Q_inv@(c + A.T@u)
+
+    # return variables x and u
+    return x, u
+
+
+# solve the linear system from KKT Conditions method
+def linear_system_solver(Q, A, c, b, dim):
     # get the linear system
-    matrix = np.block([[Q, A],
-                       [A.T, np.zeros((dim, dim))]])
+    matrix = np.block([[Q, A.T],
+                       [A, np.zeros((dim, dim))]])
     vector = np.block([[-c],
                        [b]])
 
@@ -14,28 +74,4 @@ def analytical_solver(Q, A, c, b, dim):
 
     # return variables x and u
     x, u = np.split(variables, [dim])
-    return x, u
-
-
-# solve by gradient descent from Lagrangian Duality method
-def gradient_descent_solver(Q, A, c, b, dim, alpha=0.01, tol=1e-6, max_iter=1000):
-    # precompute fixed terms
-    Q_inv = np.linalg.inv(Q)
-    g = - A @ Q_inv @ A.T
-    h = - A@Q_inv@c + b
-    gradient = lambda u: g@u + h
-
-    # gradient descent
-    u = np.zeros((dim, 1))
-    for i in range(max_iter):
-        grad = gradient(u)
-        u_new = u + alpha * grad
-        if np.linalg.norm(u_new - u) < tol:
-            break
-        u = u_new
-
-    # get value of x based on u
-    x = Q_inv@(c + A.T@u)
-
-    # return variables x and u
     return x, u
